ca28f11f86271bac667d27d62b5d9e83fa709204
AbhinavHooda <abhinavhudda@gmail.com>
2023-04-18 19:10:12 +0530

    models

--- lms/app/models.py
+++ lms/app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Categories(models.Model):
     icon = models.CharField(max_length=200, null=True)
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, default=None)
     
     def __str__(self):
         return self.name
