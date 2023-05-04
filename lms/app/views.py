d26d69e2a49e796f1f5477623d7c631a674dacda
AbhinavHooda <abhinavhudda@gmail.com>
2023-05-04 23:13:24 +0530

    none

--- lms/app/views.py
+++ lms/app/views.py
@@ -106,19 +106,21 @@ def enrolled_courses(request):
  
 @login_required
 def watchcourse(request, slug):
-    # lecture = request.GET.get('lecture')
-    # course_id = Courses.objects.get(slug=slug)
+    lecture = request.GET.get('lecture')
+    videos = video.objects.get(id=lecture)
+    course_id = Courses.objects.get(slug=slug)
     course = Courses.objects.filter(slug=slug)
-    # try:
-        # check_enroll =Usercourse.objects.get(user=request.user, course=course_id)
-        # videos = video.objects.get(id=lecture)
-    if course.exists():
-        course = course.first()
-    else:
-        return redirect('notfound')
-    # except Usercourse.DoesNotExist:
-        # return redirect('notfound')    
-    return render(request, 'app/course-watch.html',{'course':course})
+
+    try:
+        check_enroll =Usercourse.objects.get(user=request.user, course=course_id)
+        videos = video.objects.get(id=lecture)
+        if course.exists():
+            course = course.first()
+        else:
+            return redirect('notfound')
+    except Usercourse.DoesNotExist:
+        return redirect('notfound')    
+    return render(request, 'app/course-watch.html',{'course':course,'videos':videos, 'check_enroll':check_enroll})
 
 def notfound(request):
     category = Categories.get_all_categories(Categories)
