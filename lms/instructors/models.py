723eb9db413c1552c3341bbe0f435c38d5d07603
AbhinavHooda <abhinavhudda@gmail.com>
2023-05-10 16:45:48 +0530

    model fixes

--- lms/instructors/models.py
+++ lms/instructors/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils.text import slugify
+from django.db.models.signals import pre_save
 
 # Create your models here.
+class instructors(models.Model):
+    name = models.CharField(max_length=200)
+    designation = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='media/instructors/')
+    description = models.TextField()
